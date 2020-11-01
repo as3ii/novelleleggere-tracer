cf89d64b044dc28fd1a4f8dfcc69c1ea484e02b9
as3ii <as3ii777@gmail.com>
2020-11-01 19:54:33 +0100

    Improved daemon shell script

--- novelleleggere_tracer_d.sh
+++ novelleleggere_tracer_d.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 while true; do
-    python3 novelleleggere_tracer.py -r
+    # run every minute if there are problems
+    while ! python3 novelleleggere_tracer.py -r; do
+        sleep 60
+    done
     sleep 2700  # 45 minutes
 done
 
