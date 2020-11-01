b6821aec2992fe7305b23c8ed5c0cee527535230
as3ii <as3ii777@gmail.com>
2020-11-01 17:04:37 +0100

    renamed daemon script, changed sleep time

--- novelleleggere-tracer-d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-    python3 novelleleggere_tracer.py -r
-    sleep 300
-done
---
+++ novelleleggere_tracer_d.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while true; do
+    python3 novelleleggere_tracer.py -r
+    sleep 2700  # 45 minutes
+done
+
