79ae1dcf9304eb7da87efbc80fb53c56b4e9b2a4
as3ii <as3ii777@gmail.com>
2020-12-05 21:44:47 +0100

    added automatic loading of python venv and global variable management

--- novelleleggere_tracer_d.sh
+++ novelleleggere_tracer_d.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+if [ -e "${0%/*}/bin/activate" ]; then
+    . "${0%/*}/bin/activate"
+fi
+
+if [ "$TOKEN" = "" ]; then
+    export TOKEN="000000000:AAAAAA-aaaaaaaaaaaaaaaaaa_AAAAAAAAA"
+fi
+if [ "$CHATID" = "" ]; then
+    export CHATID="-1000000000000"
+fi
+if [ "$DBFILE" = "" ]; then
+    export DBFILE="database.json"
+fi
+
 while true; do
     # run every minute if there are problems
     while ! python3 novelleleggere_tracer.py -r; do
